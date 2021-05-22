22a43e3090a257693e218b96aef0dbf816a3943b
KivutiBrian <briankivuti1@gmail.com>
2021-05-22 15:53:39 +0300

    update

--- settings/db_config.py
+++ settings/db_config.py
@@ -1,9 +1,13 @@
+import os
+
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'SDFSDF'
 
 class Development(Config):
-    SECRET_KEY='SOME-KEY'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///purity.db'
+    FLASK_APP = os.environ.get("FLASK_APP")
+    FLASK_ENV = os.environ.get("FLASK_ENV")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     DEBUG=True
 
 # postgresql://postgres:Brian8053@@127.0.0.1:5432/payrollmanager
